b54bf37e4a14c96c0c12f60a4fddc2f58868a820
Ali Dikici <aliriza.dikici@gmal.com>
2014-10-12 15:13:38 +0400

    Commit 2

--- server/config/passport.js
+++ server/config/passport.js
@@ -19,6 +19,60 @@ module.exports = function(passport){
 		});
 		*/
 	});
+	
+	passport.use('local-signup', new LocalStrategy({
+			// by default, local strategy uses username and password, we will override with email
+			usernameField : 'email',
+			passwordField : 'passwd',
+			passReqToCallback : true // allows us to pass back the entire request to the callback
+		},
+		function(req, email, password, done) {
+			// asynchronous
+			// User.findOne wont fire unless data is sent back
+			process.nextTick(function() {
+				// find a user whose email is the same as the forms email
+				// we are checking to see if the user trying to login already exists
+				
+				users.findOne({ 'user' :  email }, function(err, user) {
+					// if there are any errors, return the error
+					if (err)
+						return done(err);
+					// check to see if theres already a user with that email
+					if (user) {
+						return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+					} else {
+						// if there is no user with that email	
+						// create the user
+						var passHash = generateHash(password);
+						console.log(passHash);
+						users.insert({user: email, pass: passHash }, function(err, userInserted){
+							if(err)
+								return done(err);
+							if(userInserted)
+								return done(null, userInserted, req.flash('signupMessage', 'You have succesfully registered'));
+						});
+						
+						
+						//return done(null, true, req.flash('signupMessage', 'You have succesfully registered'));
+						
+						/*var newUser            = new User();
+
+						// set the user's local credentials
+						newUser.local.email    = email;
+						newUser.local.password = newUser.generateHash(password);
+
+						// save the user
+						newUser.save(function(err) {
+							if (err)
+								throw err;
+							return done(null, newUser);
+						});
+						*/
+					}
+				});    
+			});
+		}
+	));
 }
 
 
