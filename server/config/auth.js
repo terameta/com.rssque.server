5a9e65bc5599e0c7c4efb0f07a5b4919fbeb5ee8
Ali Dikici <aliriza.dikici@gmal.com>
2014-10-12 13:03:48 +0400

    Facebook Auth

---
+++ server/config/auth.js
@@ -0,0 +1,7 @@
+module.exports{
+	'facebookauth':{
+		'clientID'		: '***REMOVED***',
+		'clientSecret'	: '***REMOVED***',
+		'callbackURL' 	: 'http://dev.rssque.com:3000/auth/facebook/callback'
+	}
+}
