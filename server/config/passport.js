df58c63fb5d01f9ed93c1f98e1388a2ee1400c85
Ali Dikici <aliriza.dikici@gmal.com>
2014-10-12 15:52:02 +0400

    Passport Changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -58,24 +58,23 @@ module.exports = function(passport){
 			});
 		}
 	));
+	
+	passport.use(new LocalStrategy({
+			usernameField: 'email',
+			passwordField: 'passwd',
+			passReqToCallback : true
+		},
+		function(req, email, password, done){
+			users.findOne({ user: email }, function(err, user) {
+				if (err) { return done(err); }
+				if (!user) {
+					return done(null, false, req.flash( 'loginMessage', 'Incorrect username.'));
+				}
+				if (user.password == generateHash(password)) {
+					return done(null, false, req.flash( 'loginMessage', 'Incorrect password.'));
+				}
+				return done(null, user);
+			});
+		}
+	));
 }
-
-/*
-passport.use(new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'passwd',
-		passReqToCallback : true
-	},
-	function(req, email, password, done){
-		users.findOne({ user: email }, function(err, user) {
-			if (err) { return done(err); }
-			if (!user) {
-				return done(null, false, req.flash( 'loginMessage', 'Incorrect username.'));
-			}
-			if (user.password == generateHash(password)) {
-				return done(null, false, req.flash( 'loginMessage', 'Incorrect password.'));
-			}
-			return done(null, user);
-		});
-	}
-));*/
