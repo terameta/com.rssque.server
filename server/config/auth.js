0fe68ab1a81baba93ae1ad67dd58f3e0613da752
Ali Dikici <aliriza.dikici@gmal.com>
2014-10-12 13:09:13 +0400

    no message

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,5 +3,17 @@ module.exports{
 		'clientID'		: '***REMOVED***',
 		'clientSecret'	: '***REMOVED***',
 		'callbackURL' 	: 'http://dev.rssque.com:3000/auth/facebook/callback'
+	},
+	
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://dev.rssque.com:3000/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://dev.rssque.com:3000/auth/google/callback'
 	}
 }
