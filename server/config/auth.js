8881cd05fd594b93d7de1b7a8d282d616f7ad52a
Ali Dikici <aliriza.dikici@gmail.com>
2022-05-11 21:55:46 +0400

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,18 +2,18 @@ module.exports = {
     'facebookAuth' : {
         'clientID'          : '***REMOVED***',
         'clientSecret'      : '***REMOVED***',
-        'callbackURL'       : 'http://dev.rssque.com:3000/auth/facebook/callback'
+        'callbackURL'       : '***REMOVED***'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'CQIkdWS6JgeaQJMriMbznzSx9',
-        'consumerSecret'    : 'vmGVsOOmyJ8oJ1LMH3lUGgMB3KziBA8vhCukCxwKHPYoThcgGI',
-        'callbackURL'       : 'http://dev.rssque.com:3000/auth/twitter/callback'
+        'consumerKey'       : '***REMOVED***',
+        'consumerSecret'    : '***REMOVED***',
+        'callbackURL'       : '***REMOVED***'
     },
 
     'googleAuth' : {
         'clientID'          : '***REMOVED***',
         'clientSecret'      : '***REMOVED***',
-        'callbackURL'       : 'http://dev.rssque.com:3000/auth/google/callback'
+        'callbackURL'       : '***REMOVED***'
     }
 };
